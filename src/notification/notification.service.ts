985ed3b36ee6bd482b51a58c5f7e4bc9d13a566d
Md.Navidul Hoque <hnavidul@gmail.com>
2025-05-25 17:59:08 +0600

    feat: add Redis support and implement notification system with model, service, and module integration

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,16 +1,34 @@
+import { InjectQueue } from "@nestjs/bull";
 import { Injectable } from "@nestjs/common";
+import { Queue } from "bull";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class NotificationService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    @InjectQueue("notification-queue") private notificationQueue: Queue
+  ) { }
 
   async createNotification(userId: string, content: string) {
-    await this.prisma.notifications.create({
+    await this.prisma.notification.create({
       data: {
         userId,
         content,
-      },
+      }
     });
+  }
+
+  async sendNotifications(userId: string, content: string, delay: number = 0) {
+
+    await this.notificationQueue.add(
+      'send-notification',
+      { userId, content },
+      {
+        delay,
+        attempts: 3,           // retry up to 3 times if the job fails
+        removeOnComplete: true // remove from queue after success
+      }
+    );
   }
 }
