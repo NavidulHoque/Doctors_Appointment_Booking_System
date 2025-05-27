8afc48748bc5c0224b1964e29de68d2c1d2efd22
Md.Navidul Hoque <hnavidul@gmail.com>
2025-05-27 21:20:10 +0600

    feat: enhance payment processing; add user role check in session creation and improve error handling for appointment retrieval

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,13 +1,25 @@
 // src/payment/appointment-payment.controller.ts
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { AuthGuard } from 'src/auth/guard';
+import { UserDto } from 'src/user/dto';
+import { User } from 'src/user/decorator';
+import { CheckRoleService } from 'src/common/checkRole.service';
 
+@UseGuards(AuthGuard)
 @Controller('payment')
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
+  constructor(
+    private readonly paymentService: PaymentService,
+    private readonly checkRoleService: CheckRoleService
+  ) {}
 
   @Post('create-session')
-  async createSession(@Body() body: { appointmentId: string; userId: string; amount: number }) {
+  async createSession(
+    @Body() body: { appointmentId: string; userId: string; amount: number },
+    @User() user: UserDto
+  ) {
+    this.checkRoleService.checkIsPatient(user.role);
     return this.paymentService.createPaymentSession(body.appointmentId, body.userId, body.amount);
   }
 
